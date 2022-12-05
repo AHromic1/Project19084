6115d446ea4c589d6ff70c3af752fb8b9dc81c36
Amina <aminahromic@gmail.com>
2022-12-05 01:37:13 +0100

    Initial commit

---
+++ src/test/java/ba/unsa/etf/rpr/ProjectTesting.java
@@ -0,0 +1,13 @@
+package ba.unsa.etf.rpr;
+
+import ba.unsa.etf.rpr.*;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * contains unit tests for relavant parts of ExpressionEvaluator class
+ */
+
+public class ProjectTesting {
+}
