c59f6c0545203a04653b81737335c2cbdb2f546d
Amina <aminahromic@gmail.com>
2022-12-11 15:35:30 +0100

    ArtistsDao layer completed

--- src/main/java/ba/unsa/etf/rpr/dao/ArtistsDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/ArtistsDao.java
@@ -2,9 +2,25 @@ package ba.unsa.etf.rpr.dao;
 
 import ba.unsa.etf.rpr.domain.Artists;
 
+import java.util.List;
+
 /**
- * an interface for artists which extends Dao
+ * an interface for artists which extends Dao with methods specific for artists table
  */
 public interface ArtistsDao extends Dao<Artists> {
+    @Override  //da li je override potreban ?
+     Artists getById(int id) ;
+
+    @Override
+     Artists add(Artists item);
+
+    @Override
+     Artists update(Artists item);
+
+    @Override
+     void delete(int id);
+    @Override
+     List<Artists> getAll() ;
+
 
 }
