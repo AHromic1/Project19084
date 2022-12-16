14d3c346bfa5932dd6a8cd4d80bf228abcedf104
Amina <aminahromic@gmail.com>
2022-12-17 00:58:10 +0100

    CustomersSQLImplementation layer completed

--- src/main/java/ba/unsa/etf/rpr/dao/CustomersSQLImplementation.java
+++ src/main/java/ba/unsa/etf/rpr/dao/CustomersSQLImplementation.java
@@ -1,9 +1,155 @@
 package ba.unsa.etf.rpr.dao;
+import ba.unsa.etf.rpr.domain.Artists;
 import ba.unsa.etf.rpr.domain.Customers;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
-public class CustomersSQLImplementation {
+public class CustomersSQLImplementation implements Dao<Customers> {
+    private Connection connection;
+
+    public CustomersSQLImplementation(){
+        try{
+            this.connection = DriverManager.getConnection("jdbc:mysql://sql7.freemysqlhosting.net", "sql7582777", "RUnD8aSmIE");
+            //connects to a database
+            //url: jdbc:mysql:// + my link
+        }
+        catch (Exception e){
+            e.printStackTrace();
+            //shows where the exception occured
+        }
+
+    }
+
+    @Override
+    public Customers getById(int id) {
+        String query = "SELECT * FROM Customers WHERE Customer_id = ?";
+        try{
+            PreparedStatement stmt = this.connection.prepareStatement(query);
+            stmt.setInt(1, id);
+            ResultSet rs = stmt.executeQuery();
+
+            if (rs.next()){ // result set is iterator. uslov?
+                Customers customer= new Customers();
+                customer.setCustomer_id(rs.getInt(1));
+                customer.setFirst_name(rs.getString(2));  //pokušaj
+                customer.setLast_name(rs.getString(3));
+                customer.setPhone_number(rs.getString(4));
+                customer.setAddress(rs.getString(5));
+                customer.setArt_FK(rs.getInt(6));
+                rs.close();
+                return customer;
+            }
+            else{
+                return null; // if there is no elements in the result set return null
+            }
+        }
+        catch (SQLException e){
+            e.printStackTrace(); // poor error handling
+        }
+        return null;
+        //ZBOG ECXEPTION, on ce ispisati tekst stack trace, ali treba da nesto vrati
+    }
+
+
+    @Override
+    public Customers add(Customers item) {
+        String insert = "INSERT INTO Customers(First_name, Last_name, Phone_number, Address, Art_FK) VALUES(?,?,?,?,?)";
+
+        try{
+            PreparedStatement stmt = this.connection.prepareStatement(insert, Statement.RETURN_GENERATED_KEYS);
+            //generated keys slicno kao auto increment??? - da, to je id
+
+            stmt.setString(1, item.getFirst_name());
+            stmt.setString(2, item.getLast_name());
+            stmt.setString(3, item.getPhone_number());
+            stmt.setString(4, item.getAddress());
+            stmt.setInt(5, item.getArt_FK());
+
+            stmt.executeUpdate(); //ovdje treba jer nismo rs preko toga uradili?
+
+            ResultSet rs = stmt.getGeneratedKeys();
+            //zasto rs.next
+            rs.next(); // we know that there is one key
+            //prelazimo u željeni red i dodajemo
+            item.setCustomer_id(rs.getInt(1)); //set id to return it back
+
+            //setamo samo id ?
+            return item;  //zašto smo morali da vraćamo item?
+        }
+        catch (SQLException e){
+            e.printStackTrace();
+        }
+        return null;   //ZASTO???  ybog exception
+    }
+
+    @Override
+    public Customers update(Customers item) {
+        String update = "UPDATE Customers SET First_name = ?, Last_name = ?, Phone_number = ?, Address = ?, Art_FK = ?   WHERE Customer_id = ?";
+        try{
+            PreparedStatement stmt = this.connection.prepareStatement(update, Statement.RETURN_GENERATED_KEYS);
+            stmt.setObject(1, item.getFirst_name());
+            stmt.setObject(2, item.getLast_name());
+            stmt.setObject(3, item.getPhone_number());
+            stmt.setObject(4, item.getAddress());
+            stmt.setObject(5, item.getArt_FK());
+
+            stmt.executeUpdate();
+            //isto kao i prethodno, ali bez rs jer nam ne treb ijedna nova informacija, već znamo id
+
+            return item;  //zasto vracamo ista?
+        }
+        catch (SQLException e){
+            e.printStackTrace();
+            return null;
+        }
+
+
+    }
+
+    @Override
+    public void delete(int id) {
+        String delete = "DELETE FROM Customers WHERE Customer_id = ?";
+        try{
+            PreparedStatement stmt = this.connection.prepareStatement(delete, Statement.RETURN_GENERATED_KEYS);
+            stmt.setObject(1, id);
+            //ustvari, puni se ova statement, koja će zatim nesto uraditi
+
+            stmt.executeUpdate();
+            /*Invokes the SQL command contained within the prepared statement. This must be INSERT, UPDATE, DELETE, or a command that returns nothing.*/
+        }
+        catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public List<Customers> getAll() {
+        String query = "SELECT * FROM Customers";
+        List<Customers> customers = new ArrayList<Customers>();
+        try{
+            PreparedStatement stmt = this.connection.prepareStatement(query);
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next()){ // result set is iterator.
+                Customers customer = new Customers();
+                customer.setCustomer_id(rs.getInt(1));
+                customer.setFirst_name(rs.getString(2));  //pokušaj
+                customer.setLast_name(rs.getString(3));
+                customer.setPhone_number(rs.getString(4));
+                customer.setAddress(rs.getString(5));
+                customer.setArt_FK(rs.getInt(6));
+                customers.add(customer);
+            }
+            rs.close();
+        }
+        catch (SQLException e){
+            e.printStackTrace(); // poor error handling
+        }
+        return customers;
+    }
+
+
 
 
 }
--- src/main/java/ba/unsa/etf/rpr/domain/Customers.java
+++ src/main/java/ba/unsa/etf/rpr/domain/Customers.java
@@ -1,4 +1,85 @@
 package ba.unsa.etf.rpr.domain;
 
+import java.util.Objects;
+
 public class Customers {
+    private int Customer_id;
+    private String First_name;
+    private String Last_name;
+    private String Phone_number;
+    private String Address;
+    private int Art_FK;
+
+    public int getCustomer_id() {
+        return Customer_id;
+    }
+
+    public void setCustomer_id(int customer_id) {
+        Customer_id = customer_id;
+    }
+
+    public String getFirst_name() {
+        return First_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        First_name = first_name;
+    }
+
+    public String getLast_name() {
+        return Last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        Last_name = last_name;
+    }
+
+    public String getPhone_number() {
+        return Phone_number;
+    }
+
+    public void setPhone_number(String phone_number) {
+        Phone_number = phone_number;
+    }
+
+    public String getAddress() {
+        return Address;
+    }
+
+    public void setAddress(String address) {
+        Address = address;
+    }
+
+    public int getArt_FK() {
+        return Art_FK;
+    }
+
+    public void setArt_FK(int art_FK) {
+        Art_FK = art_FK;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Customers customers = (Customers) o;
+        return Customer_id == customers.Customer_id && Art_FK == customers.Art_FK && First_name.equals(customers.First_name) && Last_name.equals(customers.Last_name) && Phone_number.equals(customers.Phone_number) && Address.equals(customers.Address);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(Customer_id, First_name, Last_name, Phone_number, Address, Art_FK);
+    }
+
+    @Override
+    public String toString() {
+        return "Customers{" +
+                "Customer_id=" + Customer_id +
+                ", First_name='" + First_name + '\'' +
+                ", Last_name='" + Last_name + '\'' +
+                ", Phone_number='" + Phone_number + '\'' +
+                ", Address='" + Address + '\'' +
+                ", Art_FK=" + Art_FK +
+                '}';
+    }
 }
