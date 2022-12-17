c309ef583cc4c7b16dc22f899c9622eeeebdc25f
Amina <aminahromic@gmail.com>
2022-12-17 16:28:23 +0100

    Testing main

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -1,5 +1,9 @@
 package ba.unsa.etf.rpr;
 
+import ba.unsa.etf.rpr.dao.ArtistsSQLImplementation;
+import ba.unsa.etf.rpr.domain.Artists;
+import java.sql.Date;
+
 /**
  * Hello world!
  *
@@ -8,6 +12,16 @@ public class App
 {
     public static void main( String[] args )
     {
+        Artists artist = new Artists();
+        artist.setFirst_name("Frida");
+        artist.setLast_name("Kahlo");
+        artist.setBirthplace("Spain");
+        Date firstDate1 = new Date(2001, 11, 1);
+        artist.setDate_of_birth(firstDate1);
+
+        ArtistsSQLImplementation t = new ArtistsSQLImplementation();
+        t.add(artist);
+        ///TESTIRATI!
         System.out.println( "Hello World!" );
     }
 }
