957b3441c20cf9ebe93ff0d4b71f67a35f4c45b0
Amina <aminahromic@gmail.com>
2022-12-18 21:15:04 +0100

    controller for hello

--- src/main/java/ba/unsa/etf/rpr/controllers/Hellobutton.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/Hellobutton.java
@@ -1,4 +1,14 @@
 package ba.unsa.etf.rpr.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
 public class Hellobutton {
+    public Label labelid;
+    public Button Helloid;
+
+    public void Hello(ActionEvent actionEvent) {
+        labelid.setText("Hello!");
+    }
 }
