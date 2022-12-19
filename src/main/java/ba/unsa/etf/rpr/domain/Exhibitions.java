4cff2f8b0f8ad08eb8a856d00eee35a8fb0b64fb
Amina <aminahromic@gmail.com>
2022-12-19 14:06:02 +0100

    new dao refactor

--- src/main/java/ba/unsa/etf/rpr/domain/Exhibitions.java
+++ src/main/java/ba/unsa/etf/rpr/domain/Exhibitions.java
@@ -5,20 +5,14 @@ import java.sql.Date;
 import java.util.Objects;
 
 //generalno, kako radi?
-public class Exhibitions {
-    private int Exhibition_id;
+public class Exhibitions implements Idable{
+    private int Id;
     private String Exhibition_name;
     private Date Start_date;
     private Date End_date;
     private String Location;
 
-    public int getExhibition_id() {
-        return Exhibition_id;
-    }
 
-    public void setExhibition_id(int exhibition_id) {
-        Exhibition_id = exhibition_id;
-    }
 
     public String getExhibition_name() {
         return Exhibition_name;
@@ -57,22 +51,32 @@ public class Exhibitions {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Exhibitions that = (Exhibitions) o;
-        return Exhibition_id == that.Exhibition_id && Exhibition_name.equals(that.Exhibition_name) && Start_date.equals(that.Start_date) && End_date.equals(that.End_date) && Location.equals(that.Location);
+        return Id == that.Id && Exhibition_name.equals(that.Exhibition_name) && Start_date.equals(that.Start_date) && End_date.equals(that.End_date) && Location.equals(that.Location);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(Exhibition_id, Exhibition_name, Start_date, End_date, Location);
+        return Objects.hash(Id, Exhibition_name, Start_date, End_date, Location);
     }
 
     @Override
     public String toString() {
         return "Exhibitions{" +
-                "Exhibition_id=" + Exhibition_id +
+                "Exhibition_id=" + Id +
                 ", Exhibition_name='" + Exhibition_name + '\'' +
                 ", Start_date=" + Start_date +
                 ", End_date=" + End_date +
                 ", Location='" + Location + '\'' +
                 '}';
+    }
+
+    @Override
+    public void setId(int id) {
+        Id = id;
+    }
+
+    @Override
+    public int getId() {
+        return Id;
     }
 }
