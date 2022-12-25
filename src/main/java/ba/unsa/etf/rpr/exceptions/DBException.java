f84f035086e3c7d66e535b2a64fce1ef4735d55f
Amina <aminahromic@gmail.com>
2022-12-25 12:37:36 +0100

    my exception for a database

---
+++ src/main/java/ba/unsa/etf/rpr/exceptions/DBException.java
@@ -0,0 +1,13 @@
+package ba.unsa.etf.rpr.exceptions;
+
+/**
+ * custom exception with two different versions
+ */
+public class DBException extends Exception{
+    public DBException(String message, Exception e){
+        super(message, e);
+    }
+    public DBException(String message){
+        super(message);
+    }
+}
