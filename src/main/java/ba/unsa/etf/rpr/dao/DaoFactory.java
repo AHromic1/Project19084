f297e1dadce6e6e860b7ae1e5cfcd0adc3fe2cdf
Amina <aminahromic@gmail.com>
2022-12-25 16:32:45 +0100

    daofactory added

---
+++ src/main/java/ba/unsa/etf/rpr/dao/DaoFactory.java
@@ -0,0 +1,32 @@
+package ba.unsa.etf.rpr.dao;
+
+
+/**
+ * Factory method for singleton implementation of DAOs
+ *
+ * @author Amina Hromic (ahromic1)
+ */
+
+public class DaoFactory {  //gdje koristiti???
+   /* private static final ArtistsDao artistsDao = new ArtistsSQLImplementation(); ???
+    private static final ArtworkDao artworkDao = new ArtworkSQLImplementation();
+    private static final ExhibitionsDao exhibitionsDao = new ExhibitionsSQLImplementation();
+
+
+    private DaoFactory(){
+    }
+
+
+    public static ArtistsDao artistsDao(){
+        return artistsDao;
+    }
+
+    public static ArtworkDao artworkDao(){
+        return artworkDao;
+    }
+
+    public static ExhibitionsDao exhibitionsDao(){
+        return exhibitionsDao;
+    }*/
+
+}
