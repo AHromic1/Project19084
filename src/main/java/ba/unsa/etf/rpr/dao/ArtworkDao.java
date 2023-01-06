cf2e7ac28c78e8cbf5a073b8fc9c78c3203dc811
Amina <aminahromic@gmail.com>
2023-01-06 08:20:57 +0100

    refactoring - singleton

--- src/main/java/ba/unsa/etf/rpr/dao/ArtworkDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/ArtworkDao.java
@@ -5,20 +5,9 @@ import ba.unsa.etf.rpr.domain.Artwork;
 import java.util.List;
 
 public interface ArtworkDao extends Dao<Artwork>{
-//    @Override  //da li je override potreban ?
-//    Artwork getById(int id) ;
-//    //public is redundant
-//    @Override
-//    Artwork add(Artwork item);
-//
-//    @Override
-//    Artwork update(Artwork item);
-//
-//    @Override
-//    void delete(int id);
-//    @Override
-//    List<Artwork> getAll() ;
 
-    //u svakom Dao interfejsu se nalaze samo one metode koje su specificne za tu tabelu odnosno model klasu
+
+    //u svakom Dao interfejsu se nalaze samo one metode koje su specificne za tu
+    // tabelu odnosno model klasu
 
 }
