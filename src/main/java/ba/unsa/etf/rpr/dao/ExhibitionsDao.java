7909aaf16178667d25e4d0084f9129be37431691
Amina <aminahromic@gmail.com>
2023-02-08 17:30:17 +0100

    added javadoc

--- src/main/java/ba/unsa/etf/rpr/dao/ExhibitionsDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/ExhibitionsDao.java
@@ -7,7 +7,18 @@ import ba.unsa.etf.rpr.exceptions.DBException;
 import java.sql.Date;
 import java.util.List;
 
+/**
+ * @author Amina Hromic
+ * an interface for artists which extends Dao with methods specific for artists table
+ */
 public interface ExhibitionsDao extends Dao<Exhibitions>  {
+
+    /**
+     * a method which searches for exhibitions by date
+     * @param d date
+     * @return list of exhibitions
+     * @throws DBException when something is out of order
+     */
 
     List<Exhibitions> SearchByDate(Date d) throws DBException;
 
