aeff7701b7572ef88590f113339f7f6a95928581
Amina <aminahromic@gmail.com>
2023-02-08 17:27:46 +0100

    added javadoc

--- src/main/java/ba/unsa/etf/rpr/dao/ArtistsSQLImplementation.java
+++ src/main/java/ba/unsa/etf/rpr/dao/ArtistsSQLImplementation.java
@@ -9,6 +9,11 @@ import ba.unsa.etf.rpr.exceptions.DBException;
 import java.util.Map;
 import java.util.TreeMap;
 
+/**
+ *  @author Amina Hromic
+ * a class for implementation of methods for Artist table
+ */
+
 
 public class ArtistsSQLImplementation extends AbstractDao<Artists> implements ArtistsDao{
     private static  ArtistsSQLImplementation instance = null;
@@ -21,17 +26,32 @@ public class ArtistsSQLImplementation extends AbstractDao<Artists> implements Ar
         super("Artists");
     }
 
+    /**
+     * gets an instance of an Artist
+     * @return
+     */
+
     public static ArtistsSQLImplementation getInstance(){
         if(instance==null)
             instance = new ArtistsSQLImplementation();
         return instance;
     }
 
+    /**
+     *  removes an instance of an Artist
+     */
+
     public static void removeInstance(){
         if(instance!=null)
             instance=null;
     }
 
+    /**
+     * maps result set into object of type Artists
+     * @param rs result set
+     * @return an object of type Artist
+     * @throws DBException when something is out of order
+     */
     @Override
     public Artists row2object(ResultSet rs) throws DBException {
         try {
@@ -44,6 +64,12 @@ public class ArtistsSQLImplementation extends AbstractDao<Artists> implements Ar
             throw new DBException(e.getMessage(), e);
         }
     }
+
+    /**
+     * maps objects of type Artists into rows
+     * @param object to be mapped into row
+     * @return Map<String, Object> representing a row
+     */
 
     @Override
     public Map<String, Object> object2row(Artists object) {
