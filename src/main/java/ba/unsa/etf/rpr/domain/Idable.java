3f14c31ed937c547576e206a91122d77551b3186
Amina <aminahromic@gmail.com>
2023-02-08 17:31:23 +0100

    added javadoc

--- src/main/java/ba/unsa/etf/rpr/domain/Idable.java
+++ src/main/java/ba/unsa/etf/rpr/domain/Idable.java
@@ -1,5 +1,9 @@
 package ba.unsa.etf.rpr.domain;
 
+/**
+ * @author Amina Hromic
+ * interface used to ensure that every table has a uniform name for id ("id")
+ */
 public interface Idable {
      void setId(int id);
         int getId();
